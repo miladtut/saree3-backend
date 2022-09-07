12f023ba09deca7b5be73d39aaf761c7a96d971a
miladtut <miladtut2@gmail.com>
2022-09-07 17:29:17 +0200

    activation cancel

--- app/Http/Middleware/ActivationCheckMiddleware.php
+++ app/Http/Middleware/ActivationCheckMiddleware.php
@@ -16,10 +16,11 @@ class ActivationCheckMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $data = Helpers::requestSender();
-        if ($data['active']) {
-            return $next($request);
-        }
-        return redirect(base64_decode('aHR0cHM6Ly82YW10ZWNoLmNvbS9zb2Z0d2FyZS1hY3RpdmF0aW9u'));
+        return $next($request);
+//        $data = Helpers::requestSender();
+//        if ($data['active']) {
+//            return $next($request);
+//        }
+//        return redirect(base64_decode('aHR0cHM6Ly82YW10ZWNoLmNvbS9zb2Z0d2FyZS1hY3RpdmF0aW9u'));
     }
 }
