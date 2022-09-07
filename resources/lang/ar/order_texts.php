d6612291bc7f9aa1026461d2dac1d01a31f61f79
miladtut <miladtut2@gmail.com>
2022-09-07 16:23:49 +0200

    first commit

---
+++ resources/lang/ar/order_texts.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'searching_for_delivery_man'=>'البحث عن رجل التوصيل    ',
+    'accepted_by_delivery_man'=>'قبلها رجل التوصيل    ',
+    'preparing_in_restaurants'=>'يعالج    ',
+    'picked_up'=>'التقط    ',
+    'dashboard_order_statistics'=>'إحصائيات طلب لوحة القيادة    ',
+];
